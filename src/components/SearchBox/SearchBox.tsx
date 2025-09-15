959ee246e3403ca9c8e8631067b60593d5022560
Yngrid <yngrid.sugeylg08@gmail.com>
2025-09-15 15:30:53 -0500

    feat:Se Implementa botón de X en el buscador y se aplica estilos

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,23 +1,32 @@
 import React from "react";
-import styles from "../SearchBox/Search.module.css"
+import styles from "../SearchBox/Search.module.css";
 
-type SearchProps = {
+interface SearchBoxProps {
   search: string;
   setSearch: (value: string) => void;
-};
-
-function SearchBox({ search, setSearch }: SearchProps) {
-  
-  
-  return (
-    <input
-      type="text"
-      placeholder="Buscar productos..."
-      value={search}
-      onChange={(e) => setSearch(e.target.value)}
-      className={styles.search}
-    />
-  );
 }
+
+const SearchBox: React.FC<SearchBoxProps> = ({ search, setSearch }) => {
+  const handleClear = () => {
+    setSearch("");
+  };
+
+  return (
+    <div className={styles.searchContainer}>
+      <input
+        type="text"
+        placeholder="Buscar productos..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className={styles.search}
+      />
+      {search.length > 0 && (
+        <button onClick={handleClear} className={styles.clearButton}>
+          &times;
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default SearchBox;
