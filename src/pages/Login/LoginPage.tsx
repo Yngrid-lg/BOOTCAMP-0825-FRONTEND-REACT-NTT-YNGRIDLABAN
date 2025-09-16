54e1d086129dd2d1abfdaa1e9cbad4f68ce399bd
Yngrid <yngrid.sugeylg08@gmail.com>
2025-09-15 23:59:40 -0500

    feat: separar lógica de API del formulario

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,14 +1,21 @@
-import { ModulesRoutes } from "../../router/modules-routes";
+import { useState } from "react";
 import Form from "../../components/FormLogin/Form";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
-import styles from "../Login/Login.module.css";
+import RecoveryModal from "../../components/shared/Modal/RecoveryModal";
+import styles from "./Login.module.css";
 
 function LoginPage() {
+  const [isRecoveryVisible, setIsRecoveryVisible] = useState(false);
+
+  const openRecovery = () => setIsRecoveryVisible(true);
+  const closeRecovery = () => setIsRecoveryVisible(false);
+
   return (
     <div className={styles.LoginPage}>
       <Header />
-      <Form />
+      <Form onOpenRecovery={openRecovery} />
+      <RecoveryModal isVisible={isRecoveryVisible} onClose={closeRecovery} />
       <Footer />
     </div>
   );
